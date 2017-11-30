6e5710f839ef97d62bf04bbdff2ddc812e58a93c
hebaragiseed <hebaragiseed@gmail.com>
2017-11-30 18:29:49 +0900

    다시해보기

--- src/App.js
+++ src/App.js
@@ -8,18 +8,33 @@ class App extends Component {
   }
   
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
-    .then(potato => potato.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._setMovie()
   }
 
-  _renderMovies() {
+  _renderMovies = () => {
     const movies = this.state.movies.map((movie, lenght) => {
       return <Movie title={movie.title} poster={movie.poster} key={lenght}/>
     })
     return movies
   }
+  
+  _setMovie = async () => {
+    const movies = await this._callApi()
+    console.log(movies)
+    this.setState({
+      movies
+   })
+  }
+
+
+  _callApi = () => {
+  return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+  .then(potato => potato.json())
+  .then(json => json.data.movies)
+  .catch(err => console.log(err))
+  }
+
+
   render() {
     return (
       <div>
