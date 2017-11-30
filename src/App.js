78d201810bcd81b21a33c97391fb6307edb21319
hebaragiseed <hebaragiseed@gmail.com>
2017-11-30 15:29:47 +0900

    Movie component -> function으로

--- src/App.js
+++ src/App.js
@@ -2,34 +2,53 @@ import React, { Component } from 'react';
 import styles from './App.scss';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: 'Note Book',
-    poster: 
-    'http://mblogthumb1.phinf.naver.net/20160201_152/seulgi0417_1454298381024kF3cL_JPEG/%BF%B5%C8%AD_%B3%EB%C6%AE%BA%CF_%B8%ED%B4%EB%BB%E7_%281%29.jpg?type=w800',
-  },
-  {
-    title: 'If Only',
-    poster: 
-    'https://pbs.twimg.com/media/CyHBeAYUUAERFE5.jpg',
-  },
-  {
-    title: '500일의 썸머',
-    poster: 'http://cfile1.uf.tistory.com/image/25600750576DDE111BD9F0',
-  },
-  {
-    title: '어바웃 타임',
-    poster: 'http://cfile26.uf.tistory.com/image/236A5E4D52D6B15B03FD29'  
-  }
-]
+
 
 class App extends Component {
+  state = {
+  
+  }
+  
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({       
+        movies: [          
+          {
+            title: 'Note Book',
+            poster: 
+            'http://mblogthumb1.phinf.naver.net/20160201_152/seulgi0417_1454298381024kF3cL_JPEG/%BF%B5%C8%AD_%B3%EB%C6%AE%BA%CF_%B8%ED%B4%EB%BB%E7_%281%29.jpg?type=w800',
+          },
+          {
+            title: 'If Only',
+            poster: 
+            'https://pbs.twimg.com/media/CyHBeAYUUAERFE5.jpg',
+          },
+          {
+            title: '500일의 썸머',
+            poster: 'http://cfile1.uf.tistory.com/image/25600750576DDE111BD9F0',
+          },
+          {
+            title: '어바웃 타임',
+            poster: 'http://cfile26.uf.tistory.com/image/236A5E4D52D6B15B03FD29'  
+          },
+          {
+            title: 'BEGIN AGAIN',
+            poster: 'http://cfile21.uf.tistory.com/image/24570A3854385A6026348E'
+          },
+        ]
+      }) 
+    },4000)
+  }
+  _renderMovies() {
+    const movies = this.state.movies.map((movie, lenght) => {
+      return <Movie title={movie.title} poster={movie.poster} key={lenght}/>
+    })
+    return movies
+  }
   render() {
     return (
       <div>
-        {movies.map((movie, lenght) => {
-          return <Movie title={movie.title} poster={movie.poster} key={lenght}/>
-        })}
+       {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,33 +1,26 @@
-import React, { Component }  from 'react';
+import React from 'react';
 import styles from './Movie.scss';
 import PropTypes from 'prop-types';
 
-class Movie extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-  }
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <Poster poster={this.props.poster} />
-      </div>
-    );
-  }
+function Movie({title, poster}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <Poster poster={poster} />
+    </div>
+  )
 }
 
-class Poster extends Component {
-  static propTypes = {
-    poster: PropTypes.string.isRequired
-  }
-  render() {
-    console.log(this.props)
-    return (
-      <img src={this.props.poster} alt="" />
-    );
-  }
+function Poster({poster}) {
+  return (
+    <img src={poster} alt="" />
+  )
 }
-
+Movie.propType = {
+  movie: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+}
+Poster.propTypes = {
+  poster: PropTypes.string.isRequired
+}
 export default Movie;
