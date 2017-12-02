4c17f583163aa170c4c8bd36fcf87bec42f5a031
hebaragiseed <hebaragiseed@gmail.com>
2017-12-02 12:52:11 +0900

    ajax 다시해본것

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ class App extends Component {
   }
   
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    this._getMovies()
+  }
+  
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
     .then(potato => potato.json())
-    .then(json => console.log(json))
+    .then(json => json.data.movies)
     .catch(err => console.log(err))
   }
 
   _renderMovies() {
-    const movies = this.state.movies.map((movie, lenght) => {
-      return <Movie title={movie.title} poster={movie.poster} key={lenght}/>
+    const movies = this.state.movies.map((movie) => {
+      console.log(movie)
+      return <Movie title={movie.title} poster={movie.small_cover_image} key={movie.id}/>
     })
     return movies
   }
